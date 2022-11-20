fa37481a0d16a5b38c724db196f519cfc4aba7aa
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-20 11:04:32 +0100

    docs: App components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+import Career from './pages/Career'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <NavBar/>
+      <Career/>
+      <Footer/>
+        </div>
+  )
+}
+
+export default App
